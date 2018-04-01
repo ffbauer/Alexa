384e63b3eff6fad5e34b75a14589eecc78d18432
ffbauer <37977970+ffbauer@users.noreply.github.com>
2018-04-01 14:39:41 +0200

    Update RPi_name_port_gpio.py

--- RPi_name_port_gpio.py
+++ RPi_name_port_gpio.py
@@ -22,14 +22,14 @@ class device_handler(debounce_handler):
     """
     #TRIGGERS = {str(sys.argv[1]): int(sys.argv[2])}
     #TRIGGERS = {"office": 52000}
-    TRIGGERS = {"kitchen": 52000,"living room":51000}
+    TRIGGERS = {"roteled": 52000,"living room":51000}
 
     def act(self, client_address, state, name):
         print("State", state, "from client @", client_address)
         # GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
         # GPIO.setup(int(7), GPIO.OUT)   ## Setup GPIO Pin to OUTPUT
         # GPIO.output(int(7), state) ## State is true/false
-        if name=="kitchen":
+        if name=="roteled":
             GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
             GPIO.setup(int(7), GPIO.OUT)   ## Setup GPIO Pin to OUTPUT
             GPIO.output(int(7), state) ## State is true/false
